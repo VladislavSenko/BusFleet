a90d0094f6fc56aa64b5fa03718c310105bb9a41
VladislavSenko <vlad.senko2012@gmail.com>
2022-02-14 17:35:58 +0300

    Add project files.

---
+++ BusFleet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BusFleet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
